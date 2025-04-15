e8033220b0e16d27a84211422c602dd8dece04ea
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-04-15 15:05:04 -0500

    Project setup

---
+++ controllers/loginController.js
@@ -0,0 +1,3 @@
+exports.loginWelcomeGet = (req, res) => {
+    res.render("index", {});
+}
---
+++ routes/defaultRouter.js
@@ -0,0 +1,9 @@
+const { Router } = require("express");
+
+const loginController = require("../controllers/loginController");
+
+const defaultRouter = Router();
+
+defaultRouter.get("/", loginController.loginWelcomeGet);
+
+module.exports = defaultRouter;
