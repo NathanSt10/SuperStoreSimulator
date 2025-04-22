b218d800ad1ecb4bb0c1ac0a7ea747e559f27f1a
amezt16 <ametz@uwm.edu>
2025-04-22 13:39:42 -0500

    added an insert sql file, populating database

---
+++ sql/inserts.sql
@@ -0,0 +1,4 @@
+use super_store_sim_db;
+insert into department (`name`) 
+values ('Produce'), ('Dairy'),('Electronics'),('Frozen'),('Clothes'), ('Bakery'), ('Beauty'),
+('Pharmacy'), ('Cleaning'), ('Meat'),('Toys'),('Automotive');
