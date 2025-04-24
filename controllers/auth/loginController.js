dea982bc4f04d87781d12483636bd0bfa6a0e185
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-04-24 14:17:53 -0500

    Can add members to database

--- controllers/auth/loginController.js
+++ controllers/auth/loginController.js
@@ -1,10 +1,7 @@
 const asyncHandler = require("express-async-handler");
 const User = require("../../models/usersModel");
+const Member = require("../../models/membersModel");
 
 exports.getLoginPage = asyncHandler(async (req, res) => {
-  const users = await User.findAll();
-  res.render("auth/login", {
-    title: "SuperStore",
-    users: users,
-  });
+  res.render("auth/login");
 });
--- controllers/auth/registerController.js
+++ controllers/auth/registerController.js
@@ -1,9 +1,21 @@
 const asyncHandler = require("express-async-handler");
+const User = require("../../models/usersModel");
+const Member = require("../../models/membersModel");
 
 exports.getRegisterPage = asyncHandler(async (req, res) => {
   res.render("auth/register");
 });
 
 exports.createUser = asyncHandler(async (req, res) => {
-    res.redirect("/login");
+  const { first_name, last_name, email, phone, password } = req.body;
+  const memberID = await Member.addMember(first_name, last_name, email, phone, membership_tier=1);
+  const username = first_name.substring(0,3) + "_" + last_name.substring(0,3) + memberID;
+  // const userID = await User.addUser(username, password);
+  // if (userID) {
+  res.redirect("/login");
+  // }
+  // else {
+  //   console.error();
+  // }
 });
+
