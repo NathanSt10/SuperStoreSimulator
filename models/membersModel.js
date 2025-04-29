52772c8716fc9f72a33206176d3a6c5065954abb
kaye-s <kayes@uwm.edu>
2025-04-29 11:26:46 -0500

    Fixed update member

--- models/membersModel.js
+++ models/membersModel.js
@@ -10,22 +10,20 @@ class Members {
     const points = 350;
     const query = "INSERT INTO member (first_name, last_name, email, phone_number, membership, points) VALUES (?, ?, ?, ?, ?, ?)";
     const [result] = await db.query(query, [first_name, last_name, email, phone_number, membership_tier, points]);
-    console.log(result);
     return result.insertId;
   }
 
   static async updateMember(id, first_name, last_name) {
     const query = "UPDATE member SET first_name = ?, last_name = ? WHERE id = ?";
     const [result] = await db.query(query, [first_name, last_name, id]);
-    console.log(result);
     return result.insertId;
   }
 
   static async getMember(id) {
     const query = "SELECT first_name, last_name FROM member WHERE id = ?";
     const [result] = await db.query(query, [id]);
-    console.log(result);
-    return result.insertId;
+    console.log("result: ", result);
+    return [result];
   }
 
 }
