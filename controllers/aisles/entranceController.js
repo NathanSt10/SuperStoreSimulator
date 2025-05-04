4b5c65f8a89a09efca58fecf0813e2581f637bae
kaye-s <kayes@uwm.edu>
2025-05-04 15:58:11 -0500

    Working modal and fixed styling with icons

--- controllers/aisles/entranceController.js
+++ controllers/aisles/entranceController.js
@@ -1,10 +1,13 @@
 const asyncHandler = require("express-async-handler");
 const Produce = require("../../models/produceModel");
+const Cart = require("../../models/cartModel");
 
 exports.entranceGet = asyncHandler(async (req, res) => {
     const produce = await Produce.findAll();
+    const cart = await Cart.findAll();
     res.render("aisles/entrance", {
       title: "SuperStore",
-      produce: produce
+      produce: produce,
+      cart: cart
     });
   });
