d2e7e030100a8b6ed76a49043467ad409b8d7fcc
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-05-04 19:19:00 -0500

    id goes to all pages, cart modal works

--- controllers/aisles/bakeryController.js
+++ controllers/aisles/bakeryController.js
@@ -3,11 +3,13 @@ const Bakery = require("../../models/bakeryModel");
 const Cart = require("../../models/cartModel");
 
 exports.bakeryGet = asyncHandler(async (req, res) => {
-    const bakery = await Bakery.findAll();
-    const cart = await Cart.findAll();
-    res.render("aisles/bakery", {
-      title: "SuperStore",
-      bakery: bakery,
-      cart: cart
-    });
+  const memberid = req.params.id;
+  const bakery = await Bakery.findAll();
+  const cart = await Cart.findAll();
+  res.render("aisles/bakery", {
+    title: "SuperStore",
+    bakery: bakery,
+    cart: cart,
+    memberid: memberid,
+  });
 });
--- controllers/aisles/dairyController.js
+++ controllers/aisles/dairyController.js
@@ -3,11 +3,13 @@ const Dairy = require("../../models/dairyModel");
 const Cart = require("../../models/cartModel");
 
 exports.dairyGet = asyncHandler(async (req, res) => {
-    const dairy = await Dairy.findAll();
-    const cart = await Cart.findAll();
-    res.render("aisles/dairy", {
-      title: "SuperStore",
-      dairy: dairy,
-      cart: cart
-    });
+  const memberid = req.params.id;
+  const dairy = await Dairy.findAll();
+  const cart = await Cart.findAll();
+  res.render("aisles/dairy", {
+    title: "SuperStore",
+    dairy: dairy,
+    cart: cart,
+    memberid: memberid,
+  });
 });
