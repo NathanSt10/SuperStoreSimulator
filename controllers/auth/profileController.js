45b44ed1040865f6ca9e31d20488f7176017cc11
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-05-05 13:58:35 -0500

    Profile page design

--- controllers/auth/profileController.js
+++ controllers/auth/profileController.js
@@ -5,5 +5,5 @@ exports.getProfilePage = asyncHandler(async (req, res) => {
   const memberid = req.params.id;
   const [[member]] = await Member.getMember(memberid);
   console.log(member)
-  res.render("auth/profile", { member: member });
+  res.render("auth/profile", { member: member, memberid: memberid });
 });
