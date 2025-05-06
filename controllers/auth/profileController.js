bafa4a2c17990549741aa388f4064aec7df82a3d
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-05-05 21:30:24 -0500

    Breaking convention for the sake of finals, tale as old as time

--- controllers/auth/profileController.js
+++ controllers/auth/profileController.js
@@ -1,9 +1,11 @@
 const asyncHandler = require("express-async-handler");
 const Member = require("../../models/membersModel");
+const Orders = require("../../models/ordersModel");
 
 exports.getProfilePage = asyncHandler(async (req, res) => {
   const memberid = req.params.id;
+  const orders = await Orders.getOrders(memberid);
   const [[member]] = await Member.getMember(memberid);
-  console.log(member)
-  res.render("auth/profile", { member: member, memberid: memberid });
+  console.log(orders)
+  res.render("auth/profile", { member: member, memberid: memberid, orders: orders });
 });
