38285667e7e235152fd548637598529b540e36ce
amezt16 <ametz@uwm.edu>
2025-05-06 14:15:29 -0500

    payment start change

--- models/thankModel.js
+++ models/thankModel.js
@@ -1,11 +1,25 @@
 const db = require("./db");
 
 class Thank {
-  static async findAll(memberId) {
-    const query = "SELECT product, price, quantity FROM view_orderdetail WHERE member_id = ? ORDER BY member_order_number DESC";
+  static async getMostRecent(memberId) {
+    const query = `select product, quantity, price from super_store_sim_db.view_orderdetail where member_id = ?
+and member_order_number = (select max(member_order_number) from view_orderdetail where member_id = ?)`;
+    const [result] = await db.query(query, [memberId, memberId]);
+    return result;
+  }
+
+  static async createOrder(memberId){
+    const query = "call bag_to_order(?)";
     const [result] = await db.query(query, [memberId]);
     return result;
   }
+
+  static async createPayment(memberId, payType){
+    const query = "call create_payment(?,?)";
+    const [result] = await db.query(query, [memberId, payType]);
+    return result;
+  }
+
 }
 
 module.exports = Thank;
