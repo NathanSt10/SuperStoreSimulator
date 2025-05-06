783f4a8f11d85926864d23156317ad7fb6e90535
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-05-06 12:48:14 -0500

    fixed entrance and refactored

--- controllers/aisles/entranceController.js
+++ controllers/aisles/entranceController.js
@@ -1,7 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Produce = require("../../models/produceModel");
 const Cart = require("../../models/cartModel");
-const db = require('../../config/db.js');// may need to change
 
 exports.entranceGet = asyncHandler(async (req, res) => {
   const memberid = req.params.id;
@@ -22,20 +21,12 @@ exports.addItem = asyncHandler(async (req, res) => {
   console.log('Submitted productName:', productName);
 
   // 1. Get product_id from the database
-  const [results] = await db.query(
-    'SELECT p.id FROM product p INNER JOIN aisle_view a ON p.name = a.name WHERE a.name = ?',
-    [productName]
-  );
-
-  if (!results.length) {
-    // No matching product found
-    return res.status(404).send('Product not found');
-  }
+  const [results] = await Cart.findProduct(productName);
 
   const productId = results[0].id;
 
   // 2. Now call the stored procedure with the valid productId
-  await db.query('CALL add_to_bag(?, ?, ?)', [memberId, productId, quantity]);
+  await Cart.addToCart(memberId, productId, quantity)
 
   res.redirect(`/entrance/${memberId}`);
 });
