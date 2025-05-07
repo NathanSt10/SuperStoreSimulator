c56494e2d9c6719c933d900a1b8971af34bfabc7
AlexanderMiller21 <lightningajm@gmail.com>
2025-05-07 17:31:48 -0500

    Wouldn't let me push unless I uploaded these changes

--- controllers/aisles/entranceController.js
+++ controllers/aisles/entranceController.js
@@ -1,7 +1,6 @@
 const asyncHandler = require("express-async-handler");
 const Produce = require("../../models/produceModel");
 const Cart = require("../../models/cartModel");
-const db = require('../../config/db.js');
 
 exports.entranceGet = asyncHandler(async (req, res) => {
   const memberid = req.params.id;
@@ -22,28 +21,12 @@ exports.addItem = asyncHandler(async (req, res) => {
   console.log('Submitted productName:', productName);
 
   // 1. Get product_id from the database
-<<<<<<< HEAD
-  const [results] = await db.query(
-    'SELECT p.id FROM product p INNER JOIN aisle_view a ON p.name = a.name WHERE a.name = ?',
-    [productName]
-  );
-
-  if (!results.length) {
-    // No matching product found
-    return res.status(404).send('Product not found');
-  }
-=======
   const [results] = await Cart.findProduct(productName);
->>>>>>> 1f0cead90d04b3fd44eb62f2d4383609190cbda9
 
   const productId = results[0].id;
 
   // 2. Now call the stored procedure with the valid productId
-<<<<<<< HEAD
-  await db.query('CALL add_to_bag(?, ?, ?)', [memberId, productId, quantity]);
-=======
-  await Cart.addToCart(memberId, productId, quantity);
->>>>>>> 1f0cead90d04b3fd44eb62f2d4383609190cbda9
+  await Cart.addToCart(memberId, productId, quantity)
 
   res.redirect(`/entrance/${memberId}`);
 });
