1236c019e44cd252174aa9b9e43998da78846acb
Nathan Stout <143974156+NathanSt10@users.noreply.github.com>
2025-05-08 11:54:06 -0500

    Restock front end

--- public/modal.js
+++ public/modal.js
@@ -1,19 +1,37 @@
 window.onload = function () {
-    var modal = document.getElementById("memberModal");
-    var btn = document.getElementById("memberButton");
-    var span = document.getElementsByClassName("close")[0];
-  
-    btn.onclick = function () {
-      modal.classList.add("show");
-    }
-  
-    span.onclick = function () {
-      modal.classList.remove("show");
-    }
-  
-    window.onclick = function (event) {
-      if (event.target == modal) {
-        modal.classList.remove("show");
-      }
+  var memberModal = document.getElementById("memberModal");
+  var memberBtn = document.getElementById("memberButton");
+  var memberSpan = document.getElementsByClassName("close")[0];
+
+  memberBtn.onclick = function () {
+    memberModal.classList.add("show");
+  };
+
+  memberSpan.onclick = function () {
+    memberModal.classList.remove("show");
+  };
+
+  window.onclick = function (event) {
+    if (event.target == memberModal) {
+      memberModal.classList.remove("show");
     }
   };
+
+  var restockModal = document.getElementById("restockModal");
+  var restockBtn = document.getElementById("restockButton");
+  var restockSpan = document.getElementsByClassName("close")[1];
+  
+  restockBtn.onclick = function () {
+    restockModal.classList.add("show");
+  };
+
+  restockSpan.onclick = function () {
+    restockModal.classList.remove("show");
+  };
+
+  window.onclick = function (event) {
+    if (event.target == restockModal) {
+      restockModal.classList.remove("show");
+    }
+  };
+};
